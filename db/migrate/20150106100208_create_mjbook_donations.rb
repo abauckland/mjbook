8ceb9583c23d6c0db3e2d2c7f3a2c7f1a3dff970
andrew <admin@myhq.org.uk>
2015-01-06 10:36:08 +0000

    amend migration

--- db/migrate/20150106100208_create_mjbook_donations.rb
+++ db/migrate/20150106100208_create_mjbook_donations.rb
@@ -2,10 +2,10 @@ class CreateMjbookDonations < ActiveRecord::Migration
   def change
     create_table :mjbook_donations do |t|
       t.integer :donor_id
-      t.string :participant_id
+      t.integer :participant_id
       t.integer :event_id
       t.timestamp :date
-      t.decimal :total
+      t.decimal :total, :precision => 8, :scale => 2, default: 0.00
 
       t.timestamps
     end
