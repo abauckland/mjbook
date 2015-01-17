be9f98b237329b6a47836edf1794c44819748a65
Andrew Burton <abauckland@gmail.com>
2015-01-17 16:23:33 +0000

    omit scope

--- app/controllers/mjbook/employees_controller.rb
+++ app/controllers/mjbook/employees_controller.rb
@@ -47,7 +47,7 @@ module Mjbook
         end
 
      else
-       @expenses = Expense.company(current_user).personal
+       @expenses = Expense.company(current_user).where(:exp_type => :personal)
      end
      
      #selected parameters for filter form     
