c06f0d37a75188d57457426e38c1a3afa92c3358
Andrew Burton <abauckland@gmail.com>
2015-01-19 19:00:05 +0000

    remove whitespace

--- app/controllers/mjbook/concerns/print_document_style.rb
+++ app/controllers/mjbook/concerns/print_document_style.rb
@@ -7,7 +7,7 @@ module PrintDocumentStyle
       company_format = company_style.merge(:align => :left)
     end
     
-    def price_array
+    def price_array(pdf)
     price_array = pdf.make_table([["Price"], ["(ex VAT)"]],
                                  :column_widths => [18.mm],
                                  :cell_style => {:padding => [0.mm, 0.mm, 0.mm, 0.mm], :border_width => [0,0,0,0], :size => 7, :align => :right}
@@ -17,7 +17,7 @@ module PrintDocumentStyle
                                  end
     end
     
-    def total_array
+    def total_array(pdf)
     total_array = pdf.make_table([["Total"], ["(inc VAT)"]],
                                  :column_widths => [18.mm],
                                  :cell_style => {:padding => [0.mm, 0.mm, 0.mm, 0.mm], :border_width => [0,0,0,0], :size => 7, :align => :right}
