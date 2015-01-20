9e480ef734030bc79b914a409baba9f65ccf90e1
Andrew Burton <abauckland@gmail.com>
2015-01-20 21:14:16 +0000

    add business policy outside mjbook namespace

---
+++ app/policies/business_policy.rb
@@ -0,0 +1,31 @@
+class BusinessPolicy < Struct.new(:user, :business)
+
+    def index?
+      true
+    end
+
+    def show?
+      user.owner? || user.admin?
+    end
+
+    def new?
+      true
+    end
+
+    def edit?
+      show?
+    end
+
+    def create?
+      true
+    end
+
+    def update?
+      show?
+    end
+
+    def destroy?
+      show?
+    end
+
+end
