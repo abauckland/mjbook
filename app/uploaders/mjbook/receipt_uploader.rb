cef99a484a80ce77cbd3e6a70f8acb08bd2555e4
Andrew Burton <abauckland@gmail.com>
2015-01-21 09:34:16 +0000

    change image path

--- app/uploaders/mjbook/receipt_uploader.rb
+++ app/uploaders/mjbook/receipt_uploader.rb
@@ -7,7 +7,7 @@ module Mjbook
   storage :fog
 
   def store_dir
-    "uploads/receipts/#{current_user.company_id}/#{current_user.id}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/receipts/#{model.user.company_id}/#{model.user_id}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process :resize_to_limit => [1000, 1000]  
