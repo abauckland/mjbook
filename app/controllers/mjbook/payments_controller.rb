71c2374cc4631f2c50c3ffa1ecaa009de7ccbf49
Andrew Burton <abauckland@gmail.com>
2015-01-25 12:21:05 +0000

    correct query

--- app/controllers/mjbook/payments_controller.rb
+++ app/controllers/mjbook/payments_controller.rb
@@ -313,8 +313,8 @@ module Mjbook
       end
 
       def create_summary_record(payment)
-        last_transaction = policy_scope(Summary).where('date <= ?', expend.date).order('created_at').last
-        last_account_transaction = policy_scope(Summary).where('companyaccount_id = ? AND date <= ?', expend.account_id, expend.date).order('created_at').last
+        last_transaction = policy_scope(Summary).where('date <= ?', payment.date).order('created_at').last
+        last_account_transaction = policy_scope(Summary).where('companyaccount_id = ? AND date <= ?', payment.companyaccount_id, payment.date).order('created_at').last
 
         if last_transaction.blank?
           new_balance = payment.total
