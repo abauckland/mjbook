ad14b5f798c37447a95275c75089294d2723e594
Andrew Burton <abauckland@gmail.com>
2015-03-02 20:35:27 +0000

    add query to get invoice ref

--- app/controllers/concerns/printtables/print_payment_index.rb
+++ app/controllers/concerns/printtables/print_payment_index.rb
@@ -3,24 +3,25 @@ module Printtables
 
    # HACK the helper is included in order to allow the formatting of data for processing by prawn
     include ActionView::Helpers::NumberHelper
-      
+
     def payment_column_widths
       [18.mm, 30.mm, 31.mm, 31.mm, 18.mm, 18.mm, 18.mm, 18.mm, 94.mm]
     end
-  
+
     def payment_headers(price_array, total_array)
       ["Ref", "Income Type", "Income Ref", "Payment Method", "Paid Into", "Date", price_array, "VAT", total_array, "Notes"]
     end
-  
+
     def payment_data(data, rows)
         data.each_with_index do |set, i|
 
           if set.invoice?
-            income_ref = set.paymentitem.inline.ingroup.invoice.ref
-          elsif set.transfer?
-            income_ref = set.paymentitem.transfer.ref
-          else
-            income_ref = set.paymentitem.miscincome.ref
+            invoice = Invoice.joins(:ingroups => [:inlines => :paymentitems]).where('mjbook_paymentitems.payment_id' => set.id).first
+            if invoice
+              invoice_ref = invoice.ref
+            else
+              invoice_ref = ''
+            end
           end
 
           rows[i+1] = [
@@ -38,6 +39,6 @@ module Printtables
                        ]
         end 
     end
-  
+
   end
 end
