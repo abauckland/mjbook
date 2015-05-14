6b0770a2337e7a736bbf7beba144aa5fbeb98604
Andrew Burton <abauckland@gmail.com>
2015-05-14 13:26:01 +0100

    update helpers

--- app/helpers/mjbook/companyaccounts_helper.rb
+++ app/helpers/mjbook/companyaccounts_helper.rb
@@ -1,10 +1,19 @@
 module Mjbook
   module CompanyaccountsHelper
-    def companyaccount_in_use(companyaccount)      
+    def companyaccount_in_use(companyaccount)
       expend = Expend.where(:companyaccount_id => companyaccount.id).first
-      payment = Payment.where(:companyaccount_id => companyaccount.id).first      
+      payment = Payment.where(:companyaccount_id => companyaccount.id).first
       if expend.blank? && payment.blank?
         true
+      end
+    end
+
+    def companyaccount_cancel
+      companyaccounts = policy_scope(Companyaccount).count
+      if companyaccounts == 0
+        link_to "cancel", summaries_path
+      else
+        link_to "cancel", companyaccounts_path
       end
     end
 
