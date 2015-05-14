5197093a169b94a2b561b91335971707cca9b46b
Andrew Burton <abauckland@gmail.com>
2015-05-14 11:40:15 +0100

    Revert "add year end tracking"
    This reverts commit 72f5a594f8942ad114fc37d5e28a549e8b22f6e9.

--- app/controllers/mjbook/application_controller.rb
+++ app/controllers/mjbook/application_controller.rb
@@ -2,8 +2,6 @@ module Mjbook
   class Mjbook::ApplicationController < ActionController::Base
    layout "mjbook/books"
    include Pundit
-
-
             
       def clean_text(value)
         @value = value 
@@ -28,43 +26,6 @@ module Mjbook
         @value = @value.to_d
       end
 
-
-
-      def accounting_period(date)
-        period = policy_scope(Period).where("year_start >= ? AND year_start < ?", date, 1.year.ago(date)).first
-
-        #if record does not exist create period record
-        if period.blank?
-
-          last_period = policy_scope(Period).order(:date).last
-
-          if date > last_period
-            difference = (date - last_period.year_start) / 1.year
-            years_difference = (difference - 0.5).round
-
-            start_date = years_difference.year.from_now(last_period.year_start)
-          #if date < first_period
-          else
-            first_period = policy_scope(Period).order(:date).first
-
-            difference = (first_period.year_start - date) / 1.year
-            years_difference = (difference - 0.5).round
-
-            start_date = years_difference.year.ago(first_period.year_start)
-          end
-
-          start_year = start_date.strftime("%Y")
-          end_year = 1.year.from_now(start_date).strftime("%Y")
-          period_name = start_year + "/" + end_year
-
-          #create record for period
-          period = Period.create(:company_id => current_user.company_id,
-                                 :period => period_name,
-                                 :year_start => start_date,
-                                 :amount => 0
-                                 )
-        end
-      end
 
   end
 end
