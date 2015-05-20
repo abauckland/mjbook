0df609a6a2af3e0bb59f1c8743addde8506102d2
andrew <admin@myhq.org.uk>
2015-05-20 11:33:20 +0100

    update setting ui

--- app/helpers/mjbook/setups_helper.rb
+++ app/helpers/mjbook/setups_helper.rb
@@ -27,11 +27,11 @@ module Mjbook
       company_setting = Mjbook::Setting.where(:company_id => current_user.company_id).first
       accounts_exist = Mjbook::Companyaccount.where(:company_id => current_user.company_id).first
       if company_setting.blank?
-        link_to "continue", new_setting_path
+        link_to "continue", new_setting_path, class: "left_edit_button"
       elsif accounts_exist.blank?
-        link_to "continue", new_companyaccount_path
+        link_to "continue", new_companyaccount_path, class: "left_edit_button"
       else
-        link_to "continue", edit_mileagemode_path(current_user.company_id)
+        link_to "continue", edit_mileagemode_path(current_user.company_id), class: "left_edit_button"
       end
     end
 
