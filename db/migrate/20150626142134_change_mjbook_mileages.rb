12a18526f2976c05247ececbc653508ea19f00ff
Andrew Burton <abauckland@gmail.com>
2015-06-26 14:30:46 +0100

    changes following re allocation of mileage to expenses

---
+++ db/migrate/20150626142134_change_mjbook_mileages.rb
@@ -0,0 +1,10 @@
+class ChangeMjbookMileages < ActiveRecord::Migration
+  def change
+    remove_column :mjbook_mileages, :travel_date
+    remove_column :mjbook_mileages, :user_id
+    remove_column :mjbook_mileages, :project_id
+
+    add_column :mjbook_mileages, :expense_id, :integer
+
+  end
+end
