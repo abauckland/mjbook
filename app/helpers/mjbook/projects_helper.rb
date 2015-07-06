d621a90ab3523848da4de2d0942d65337ad8a666
Andrew Burton <abauckland@gmail.com>
2015-07-06 09:01:58 +0100

    remove mileage model reference

--- app/helpers/mjbook/projects_helper.rb
+++ app/helpers/mjbook/projects_helper.rb
@@ -3,7 +3,6 @@ module Mjbook
 
     def project_in_use(project)
       expense = Expense.where(:project_id => project.id).first
-      mileage = Mileage.where(:project_id => project.id).first
       invoice = Invoice.where(:project_id => project.id).first
       quote = Quote.where(:project_id => project.id).first
       
